eb487ab44d8d632c043b1e13834cb56e3ce49db0
Alexey Khoroshenkiy <alexeyk4@nycstudents.net>
2025-10-07 17:49:27 +0000

    done

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -7,6 +7,11 @@ public class CalculatorRunner {
         //1. create an instance of the calculator class
 
         //2. call any methods of that class to test
-
+        Calculator calc = new Calculator("calc stands for calculator by the way");
+        System.out.println(calc.performOperation("*", 2, 3));
+        System.out.println(calc.divisibleBy(5, 2));
+        System.out.println(calc.coordinatePair(0, 1864));
+        System.out.println(calc.absoluteValue(5, 10));
+        System.out.println(calc.average(10, 5));
     }
 }
